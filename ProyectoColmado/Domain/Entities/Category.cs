38a553334a6ae02b4e59a61f062767c8a6ca011b
josemiliriano <josemiliriano83@gmail.com>
2026-06-14 18:10:44 -0400

    Creacion de entidades del proyecto Colmado

---
+++ ProyectoColmado/Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int IdCategory { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public char Isdelete { get; set; } = '0';
+    }
+}
---
+++ ProyectoColmado/Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int IdProduct { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public int Stock { get; set; }
+        [ForeignKey("IdCategory")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public char IsDelete { get; set; } = '0';
+    }
+}
